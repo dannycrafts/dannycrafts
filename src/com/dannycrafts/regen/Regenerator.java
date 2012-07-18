5284117c83172cd3bb3f7e4f468cd16c5b1eda5a
Danny de Jong <magnificence@live.nl>
2012-07-18 18:57:33 +0000

    Added the collection of TouchedChunkNote to Regenerator.

--- src/com/dannycrafts/regen/Regenerator.java
+++ src/com/dannycrafts/regen/Regenerator.java
@@ -1,6 +1,10 @@
 package com.dannycrafts.regen;
 
+import java.util.*;
+
+import com.dannycrafts.*;
+
 public class Regenerator
 {
-	
+	private static Map<WorldId, Map<ChunkCoords, TouchedChunkNote>> chunkNoteCollection;
 }
