1eb35f51d0183288b27a304cd2d0a172a0079640
Danny de Jong <magnificence@live.nl>
2012-07-18 18:56:43 +0000

    Created a class TouchedChunkNote.

---
+++ src/com/dannycrafts/regen/TouchedChunkNote.java
@@ -0,0 +1,14 @@
+package com.dannycrafts.regen;
+
+import com.dannycrafts.database.Resource;
+
+public class TouchedChunkNote extends Resource
+{
+
+	@Override
+	protected void load() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
