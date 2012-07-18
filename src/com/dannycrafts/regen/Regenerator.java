b1b35cda92c6cad8c3d03a1fefc0990cfbe12a59
Danny de Jong <magnificence@live.nl>
2012-07-18 19:33:21 +0000

    Created init and uninit functions for Regenerator.

--- src/com/dannycrafts/regen/Regenerator.java
+++ src/com/dannycrafts/regen/Regenerator.java
@@ -7,4 +7,14 @@ import com.dannycrafts.*;
 public class Regenerator
 {
 	private static Map<WorldId, Map<ChunkCoords, TouchedChunkNote>> chunkNoteCollection;
+	
+	public static void init()
+	{
+		chunkNoteCollection = new HashMap<WorldId, Map<ChunkCoords, TouchedChunkNote>>();
+	}
+	
+	public static void uninit()
+	{
+		chunkNoteCollection = null;
+	}
 }
