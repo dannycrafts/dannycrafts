08fdddf4aeeff8a78fa2838678c0317a3d1b8098
Danny de Jong <magnificence@live.nl>
2012-07-19 06:17:01 +0000

    Added an id and constructor to TouchedChunkNote.

--- src/com/dannycrafts/regen/TouchedChunkNote.java
+++ src/com/dannycrafts/regen/TouchedChunkNote.java
@@ -1,10 +1,17 @@
 package com.dannycrafts.regen;
 
+import com.dannycrafts.*;
 import com.dannycrafts.database.Resource;
 
 public class TouchedChunkNote extends Resource
 {
-
+	public final ChunkCoords id;
+	
+	protected TouchedChunkNote( ChunkCoords id )
+	{
+		this.id = id;
+	}
+	
 	@Override
 	protected void load() throws Exception {
 		// TODO Auto-generated method stub
