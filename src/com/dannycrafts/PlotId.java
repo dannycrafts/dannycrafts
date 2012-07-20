84596ddcd0c56aa8cfe90e3235f67f8233c914e2
Danny de Jong <magnificence@live.nl>
2012-07-20 17:54:29 +0200

    Implemented 'compare' functions for PlotId.

--- src/com/dannycrafts/PlotId.java
+++ src/com/dannycrafts/PlotId.java
@@ -10,4 +10,16 @@ public class PlotId
 		this.world = world;
 		this.coords = coords;
 	}
+	
+	public boolean equals( Object other )
+	{
+		if ( other instanceof WorldId == false ) return false;
+		PlotId _other = (PlotId)other;
+		return this.world.equals( _other.world ) && this.coords.equals( _other.coords );
+	}
+	
+	public int hashCode()
+	{
+		return coords.hashCode();
+	}
 }
